21347fc2d67ed0a1c91a9647b476f87b4df7119b
ms7851 <ms7851@srmist.edu.in>
2026-03-11 05:40:21 -0400

    Create uc5.java

---
+++ uc5.java
@@ -0,0 +1,20 @@
+public class uc5 {
+	public static void main(String []  args){
+		String [] lines = {
+            String.join(" ", "    ***    " , "    ***    " , " ******   " , "    *****  "),
+            String.join(" ", "  **   **  " , "  **   **  " , " **    ** " , "  **       "),
+            String.join(" ", " **     ** " , " **     ** " , " **     **" , " **        "),
+            String.join(" ", " **     ** " , " **     ** " , " **    ** " , "  **       "),
+            String.join(" ", " **     ** " , " **     ** " , " ******   " , "    ***    "),
+            String.join(" ", " **     ** " , " **     ** " , " **       " , "       **  "),
+            String.join(" ", " **     ** " , " **     ** " , " **       " , "        ** "),
+            String.join(" ", "  **   **  " , "  **   **  " , " **       " , "       **  "),
+            String.join(" ", "    ***    " , "    ***    " , " **       " , "  *****    "),
+    };
+for (String line : lines){
+	System.out.println(line);
+}
+	}
+}
+OOPSUC5.java
+Displaying OOPSUC6.java.
